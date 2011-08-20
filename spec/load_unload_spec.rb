bb14a9f9cb9423c503a9281f1f476c7ee48eb26c
Chris Continanza <christopher.continanza@gmail.com>
2011-08-19 21:31:42 -0700

    failing spec for this not leaking

---
+++ spec/load_unload_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe SmartEnv, 'load/unload' do 
+  context "on extend" do
+    before do
+      # i got my hash pipe
+      @env = {} 
+      @env.extend SmartEnv
+
+      class PassOne < String
+        attr_accessor :one
+        def initialize(value)
+          @one = true
+          super
+        end
+      end
+    end
+
+    it "should not bleed" do
+      ENV['FOO'] = 'bar'
+      @env['FOO'] = 'bar'
+      @env.use(PassOne).when{ true }
+
+      ENV['FOO'].should_not respond_to :one
+      @env['FOO'].one.should be_true
+    end
+  end
+end
