b5667d3db3ce932b91dc8f1e1db8a6d768f112ee
Chris Continanza <christopher.continanza@gmail.com>
2011-10-06 00:05:02 -0700

    spec for ::when proxiesspec for ::when proxiesspec for ::when proxiesspec for ::when proxies

--- spec/registry_spec.rb
+++ spec/registry_spec.rb
@@ -1,24 +1,44 @@
 require 'spec_helper'
-describe SmartEnv, 'registering your own Proxies with a block' do
-  before do
-    class TestProxy
-      def initialize(key, value)
-      end
-    end
-    ENV.use(TestProxy).when { |key, value| key == 'FOO' }
-  end
-
-  it "should use the specified proxy when the block returns true" do
+shared_examples_for "all proxies" do
+  it "should use the specified proxy when the block or when returns true" do
     ENV['FOO'] = 'bar'
     ENV['FOO'].should be_a(TestProxy)
     ENV['BAR'] = 'bar'
     ENV['BAR'].should == 'bar'
     ENV['BAR'].should be_a(String)
   end
+end
 
-  it "raises error if the block doens't have two args" do
-    lambda { ENV.use(TestProxy).when { |k,v,x| name == 'FOO' } }.should raise_error
-    lambda { ENV.use(TestProxy).when { |value| name == 'FOO' } }.should raise_error
-    lambda { ENV.use(TestProxy).when { false } }.should_not raise_error
+describe SmartEnv, 'registering your own Proxies' do
+  context 'with a bloack' do
+    before do
+      class TestProxy
+        def initialize(key, value)
+        end
+      end
+      ENV.use(TestProxy).when { |key, value| key == 'FOO' }
+    end
+
+    it "raises error if the block doens't have two args" do
+      lambda { ENV.use(TestProxy).when { |k,v,x| name == 'FOO' } }.should raise_error
+      lambda { ENV.use(TestProxy).when { |value| name == 'FOO' } }.should raise_error
+      lambda { ENV.use(TestProxy).when { false } }.should_not raise_error
+    end
+  end
+
+  context 'with a class that implements ::when' do
+    before do
+      class TestProxy
+        def initialize(key, value)
+        end
+        
+        def self.when(key, value)
+          key == 'FOO'
+        end
+      end
+      ENV.use(TestProxy)
+    end
   end
 end
+
+
